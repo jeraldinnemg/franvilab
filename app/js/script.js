6d9d01b83b9aca77b4bfe8eddfcec349650a7070
Jeraldinne <jeraldinnemg@gmail.com>
2021-06-14 22:42:59 -0300

    commit

--- app/js/script.js
+++ app/js/script.js
@@ -3,28 +3,28 @@ let directo = document.querySelectorAll('.directo');
 let reactivo = document.querySelectorAll ('.reactivo');
 let disperso = document.querySelectorAll ('.disperso');
 
-
-console.log(directo);
-console.log(reactivo);
-console.log(disperso);
-
+//console.log(directo);
+//console.log(reactivo);
+//console.log(disperso);
 
 document.getElementById('cdirecto').onclick = function (){
     directo.forEach( function (directo) {
         directo.classList.add('show');
         directo.classList.remove('hide');
-
-        /*reactivo.classList.remove('show');
+    });
+    
+    reactivo.forEach(function(reactivo){
+        reactivo.classList.remove('show');
         reactivo.classList.add('hide');
-
+    });
+    
+    disperso.forEach(function(disperso){
         disperso.classList.remove('show');
-        disperso.classList.add('hide');*/
+        disperso.classList.add('hide');
+
     });
     
 }
-
-
-
 
 
 document.getElementById('creactivo').onclick = function (){
@@ -32,13 +32,19 @@ document.getElementById('creactivo').onclick = function (){
         reactivo.classList.remove('hide');
         reactivo.classList.add('show');
 
-        /* directo.classList.remove('show');
+    });
+
+    directo.forEach (function (directo) {
+        directo.classList.remove('show');
         directo.classList.add('hide');
 
-        disperso.classList.remove('show');
-        disperso.classList.add('hide');*/  
+    });
 
-    })
+    disperso.forEach(function(disperso){
+        disperso.classList.remove('show');
+        disperso.classList.add('hide');
+
+    });
     
 }
 
@@ -48,13 +54,18 @@ document.getElementById('cdisperso').onclick = function (){
     disperso.forEach ( function (disperso) {
         disperso.classList.remove('hide');
         disperso.classList.add('show');
-
-        /*directo.classList.remove('show');
-        directo.classList.add('hide');
-    
-        reactivo.classList.remove('show');
-        reactivo.classList.add('hide');*/
     })
+
+    directo.forEach (function (directo) {
+        directo.classList.remove('show');
+        directo.classList.add('hide');
+
+    });
+
+    reactivo.forEach(function(reactivo){
+        reactivo.classList.remove('show');
+        reactivo.classList.add('hide');
+    });
     
 };
 
